e9df52fc07759cae05b43534b9d259dca6ffe838
beata <beata@emc2wave.com>
2020-04-20 17:57:32 -0700

    add test for dropdown is-active

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -20,3 +20,13 @@ it('Dropdown starts closed', () => {
 
   expect(dropdown.className).not.to.include('is-active');
 });
+
+it('After searching, dropdown opens up', () => {
+  const input = document.querySelector('input');
+  input.value = 'avengers';
+  input.dispatchEvent(new Event('input'));
+
+  const dropdown = document.querySelector('.dropdown');
+
+  expect(dropdown.className).to.include('is-active');
+});
